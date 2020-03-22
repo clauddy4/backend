91c7936d139c20da16642b73cd3a95823f85ff06
Olga Makarova <olgamakarovatvr@gmail.com>
2020-03-22 10:16:18 +0300

    add remove_duplicates

---
+++ labs/2/remove_duplicates/remove_duplicates/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace remove_duplicates
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Incorrect number of arguments!" + "\n" + "Usage remove_duplicates.exe <input string>");
+                Environment.Exit(1);
+            }
+            else
+            {
+                Console.Write(args[0].Distinct().ToArray());
+            }
+        }
+    }
+}
